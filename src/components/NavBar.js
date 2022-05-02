aa9304786e67804a2de59a5364b8beefd0a45754
“JJoaquin Reynoso <reynoso_joaquin@yahoo.com.ar>
2022-05-02 12:00:46 -0500

    Agregando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+
+function NavBar() {
+    return (
+      <div>
+          <ul className= "Nav">
+              <li className="Text">INICIO</li>
+              <li className="Text">FOTOS</li>
+              <li className="Text">CONTACTO</li>
+              <li className="Text">AYUDA</li>
+          </ul>
+      </div>
+    );
+  }
+
+  export default NavBar
